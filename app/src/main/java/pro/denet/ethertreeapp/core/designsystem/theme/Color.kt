6d898251bca9d6feee55ed0e44bbd0056485bcb9
BiryukAl <kerzemyak001@gmail.com>
2024-03-02 20:45:22 +0300

    Add design system

--- app/src/main/java/pro/denet/ethertreeapp/core/designsystem/theme/Color.kt
+++ app/src/main/java/pro/denet/ethertreeapp/core/designsystem/theme/Color.kt
@@ -9,21 +9,23 @@ val LocalColor = staticCompositionLocalOf<TreeAppColor> {
 }
 
 // Light Scheme
-val PrimaryLight = Color(0xFF0094FF)
-val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryLight = Color(0xffffd600)
+val OnPrimaryLight = Color(0xff232323)
 val SecondaryLight = Color(0xFFDEEFFF)
 val OnSecondaryLight = Color(0xFF0094FF)
 val BackgroundLight = Color(0xFFFDFCFF)
+
 val PrimaryTextLight = Color(0xFF1A1C1E)
 val SecondaryTextLight = Color(0xFF73777F)
 val SurfaceLight = Color(0xFFFDFCFF)
 
 // DarkScheme
-val PrimaryDark = Color(0xFFA2C9FF)
-val OnPrimaryDark = Color(0xFF00315B)
+val PrimaryDark = Color(0xffffd600)
+val OnPrimaryDark = Color(0xff232323)
 val SecondaryDark = Color(0xFF004881)
 val OnSecondaryDark = Color(0xFFD3E4FF)
 val BackgroundDark = Color(0xFF1A1C1E)
+
 val PrimaryTextDark = Color(0xFFE3E2E6)
 val SecondaryTextDark = Color(0xFF8D9199)
 val SurfaceDark = Color(0xFF1A1C1E)
