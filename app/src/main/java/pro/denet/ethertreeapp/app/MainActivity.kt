afc6a862919c3ca9a795175e17ff5f7c3ec44cfb
BiryukAl <kerzemyak001@gmail.com>
2024-03-04 00:36:25 +0300

    Add Screen On Activity

--- app/src/main/java/pro/denet/ethertreeapp/app/MainActivity.kt
+++ app/src/main/java/pro/denet/ethertreeapp/app/MainActivity.kt
@@ -3,35 +3,15 @@ package pro.denet.ethertreeapp.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import pro.denet.ethertreeapp.R
 import pro.denet.ethertreeapp.core.designsystem.theme.EtherTreeAppTheme
-import pro.denet.ethertreeapp.core.designsystem.theme.TreeAppTheme
-import pro.denet.ethertreeapp.core.widget.DenetTopBar
-import pro.denet.ethertreeapp.core.widget.Greeting
+import pro.denet.ethertreeapp.feature.navigateOnTree.presentation.MainNavigationOnTreeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EtherTreeAppTheme {
-                Scaffold(
-                    topBar = {
-                        DenetTopBar(
-                            title = getString(R.string.app_name)
-                        )
-                    },
-                    containerColor = TreeAppTheme.treeAppColor.background
-                ) {
-                    Surface(
-                        modifier = Modifier.padding(it)
-                    ) {
-                        Greeting("Denet company")
-                    }
-                }
+                MainNavigationOnTreeScreen()
             }
         }
     }
